5aba41b25bb5612770af555a8dba95955c37cbe3
Miks Kalnins <MiksKalnins@MaikuMori.com>
2015-10-13 21:01:25 +0300

    Emacs: don't show toolbar in console Emacs.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,11 +8,11 @@
 ;;; Code:
 
 ;; Turn off mouse interface early in startup to avoid momentary display.
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (when window-system
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (tooltip-mode -1))
+  (tooltip-mode -1)
+  (tool-bar-mode -1))
 
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
